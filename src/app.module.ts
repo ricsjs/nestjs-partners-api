05651c1d75bb8de27ccd83bd50bb6b9e56fef535
ricsjs <ricfilho0007@gmail.com>
2024-06-17 15:24:29 -0300

    feat: spot and event cruds, dockerfile and docker compose creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { EventsModule } from './events/events.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { SpotsModule } from './spots/spots.module';
 
 @Module({
-  imports: [],
+  imports: [EventsModule, PrismaModule, SpotsModule],
   controllers: [AppController],
   providers: [AppService],
 })
